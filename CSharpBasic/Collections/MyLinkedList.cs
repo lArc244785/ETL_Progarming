fb672b6a2521fccb8b5494375cf534d91bfcda56
lArc244785 <jlky8932@gmail.com>
2023-10-11 00:04:58 +0900

    LinkedList
    ● Node
    
    ● 기능 구현
     - Add
     - Find
     - Remove

---
+++ CSharpBasic/Collections/MyLinkedList.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+	public class Node <T>
+	{
+		public T value;
+		public Node<T> prev = null;
+		public Node<T> next = null;
+		public Node(T value)
+		{
+			this.value = value;
+		}
+	}
+
+
+	internal class MyLinkedList <T>
+	{
+		private Node<T> _first;
+		private Node<T> _last;
+
+		public Node<T> First => _first;
+		public Node<T> Last => _last;
+
+		#region Add
+		private bool InitNode(Node<T> node)
+		{
+			if(_first == null || _last == null)
+			{
+				_first = node;
+				_last = node;
+				return true;
+			}
+			return false;
+		}
+		public void FirstAdd(Node<T> node)
+		{
+			if (InitNode(node))
+				return;
+
+			_first.prev = node;
+			node.next = _first;
+			_first = node;
+		}
+
+		public void LastAdd(Node<T> node)
+		{
+			if (InitNode(node))
+				return;
+
+			_last.next = node;
+			node.prev = _last;
+			_last = node;
+		}
+
+		public void FrontAdd(Node<T> target,Node<T> node)
+		{
+			if (InitNode(node))
+				return;
+			
+			if(target == _first)
+			{
+				FirstAdd(node);
+				return;
+			}
+
+			Node<T> targetPrev = target.prev;
+			targetPrev.next = node;
+			node.prev = targetPrev;
+			node.next = target;
+			target.prev = node;
+		}
+
+		public void BackAdd(Node<T> target, Node<T> node)
+		{
+			if (InitNode(node))
+				return;
+
+			if(target == _last)
+			{
+				LastAdd(node);
+				return;
+			}
+
+			Node<T> targetNext = target.next;
+			target.next = node;
+			node.prev = target;
+			node.next = targetNext;
+			targetNext.prev = node;
+		}
+		#endregion
+
+		#region Find
+		public Node<T> FirstFind(Predicate<T> match)
+		{
+			Node<T> result = null;
+			Node<T> current = _first;
+			while(current != null && result == null)
+			{
+				if(match(current.value))
+				{
+					result = current;
+				}
+
+				current = current.next;
+			}
+
+			return result;
+		}
+
+		public Node<T> LastFind(Predicate<T> match)
+		{
+			Node<T> result = null;
+			Node<T> current = _last;
+			while (current != null && result == null)
+			{
+				if (match(current.value))
+				{
+					result = current;
+				}
+
+				current = current.prev;
+			}
+
+			return result;
+		}
+		#endregion
+
+		public void Remove(Node<T> node)
+		{
+			Node<T> prevNode = node.prev;
+			Node<T> nextNode = node.next;
+
+			if (prevNode != null)
+				prevNode.next = nextNode;
+			else
+				_first = nextNode;
+
+			if (nextNode != null)
+				nextNode.prev = prevNode;
+			else
+				_last = prevNode;
+
+		}
+	}
+}
--- CSharpBasic/Collections/Program.cs
+++ CSharpBasic/Collections/Program.cs
@@ -4,31 +4,40 @@ namespace Collections
 {
 	internal class Program
 	{
+		static void ShowLinkedList<T>(MyLinkedList<T> linkedList)
+		{
+			Node<T> node = linkedList.First;
+			while(node != null)
+			{
+				Console.WriteLine(node.value);
+				node = node.next;
+			}
+
+			Console.WriteLine();
+		}
+
 		static void Main(string[] args)
 		{
-			MyDynamicArray<string> data1 = new();
-			data1.Add("철수");
-			data1.Add("영희");
-			data1.Add("재욱");
-			data1.Add("나성");
-			data1.Add("김도");
+			MyLinkedList<int> list = new MyLinkedList<int>();
 
-			MyDynamicArray<string> data2 = new();
-			data2.Add("수철");
-			data2.Add("희영");
-			data2.Add("도김");
+			Node<int> removeNode = new Node<int>(235);
 
-			using(IEnumerator<string>e1 = data1.GetEnumerator())
-			using(IEnumerator<string>e2 = data2.GetEnumerator())
-			{
-				while(e1.MoveNext() && e2.MoveNext())
-				{
-					Console.WriteLine($"e1 {e1.Current}");
-					Console.WriteLine($"e2 {e2.Current}");
-				}
-				e1.Reset();
-				e2.Reset();
-			}
+			list.FirstAdd(new Node<int>(1));
+			list.FrontAdd(list.First , new Node<int>(2));
+			list.LastAdd(new Node<int>(3));
+			list.BackAdd(list.Last, new Node<int>(4));
+			list.FrontAdd(list.Last, removeNode);
+
+			ShowLinkedList<int>(list);
+
+			Console.WriteLine(list.FirstFind((x) => x == 2).value);
+			Console.WriteLine(list.LastFind((x) => x > 3).value);
+
+			Console.WriteLine();
+
+			list.Remove(removeNode);
+
+			ShowLinkedList<int>(list);
 
 		}
 	}
